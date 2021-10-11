12c007b133e75d1436487cabc4119ee34be8ae3e
deepudo <92288099+deepudo@users.noreply.github.com>
2021-10-11 10:55:09 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -20,6 +20,12 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     OWNER = config("OWNER_ID", default=1322549723, cast=int)
     LOG = config("LOG_CHANNEL", cast=int)
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
+    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
+    MUST_JOIN = os.environ.get('MUST_JOIN', None)
+    if MUST_JOIN.startswith("@"):
+        MUST_JOIN = MUST_JOIN.replace("@", "")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
